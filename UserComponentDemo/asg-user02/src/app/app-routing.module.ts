4a4be78dfd27f16519a157bffdbc52bc19c5b049
MadhavT-1 <62096719+MadhavT-1@users.noreply.github.com>
2022-01-11 10:18:14 +0530

    commit

--- UserComponentDemo/asg-user02/src/app/app-routing.module.ts
+++ UserComponentDemo/asg-user02/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { DeleteUserComponent } from './user/delete-user/delete-user.component';
+import { FindUserComponent } from './user/find-user/find-user.component';
+import { UpdateUserComponent } from './user/update-user/update-user.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserComponent } from './user/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', redirectTo : 'alluser', pathMatch : 'full'},
+  {path : 'alluser', component : UserListComponent},
+  {path : 'adduser', component : AddUserComponent},
+  {path : 'finduser', component : FindUserComponent},
+  {path : 'updateuser', component : UpdateUserComponent},
+  {path : 'deleteuser', component : DeleteUserComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UserComponentDemo/asg-user02/src/app/app.module.ts
+++ UserComponentDemo/asg-user02/src/app/app.module.ts
@@ -9,13 +9,28 @@ import { UserListComponent } from './user/user-list/user-list.component';
 import { UserService } from './user/service/user.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { FooterComponent } from './dashboard/footer/footer.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { FindUserComponent } from './user/find-user/find-user.component';
+import { UpdateUserComponent } from './user/update-user/update-user.component';
+import { DeleteUserComponent } from './user/delete-user/delete-user.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    UserListComponent
+    UserListComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    AddUserComponent,
+    FindUserComponent,
+    UpdateUserComponent,
+    DeleteUserComponent
   ],
 
   imports: [
